5da22f7374188549f50d89de56a64ce513aba19c
Michal Stancik <michal.von.stancik@gmail.com>
2021-07-29 16:04:47 +0200

    akutne prerobenie programu na pevne casy napustania po pokazeni senzoru % nadoby

--- work.ino
+++ work.ino
@@ -20,8 +20,9 @@
 // 2020-06-07 include timelib.h namiesto time.h koli chybe v appke, VODA_PRAH na 1000
 // 2020-07-05 voda prah 900, interval na novu skusku hladin 30 minut
 // 2021-04-11 nastavenie modu polievania (vobec, vecer, rano aj vecer) pri zapinani
+// 2021-08-29 akutne prerobenie programu na pevne casy napustania po pokazeni senzoru % nadoby
 
-#define VERSION 20200705
+#define VERSION 20210829
 
 #include <TimeLib.h>
 #include <LiquidCrystal.h>
@@ -66,7 +67,9 @@ int zavlahaRano[2] = {06, 00}; // cas rannej zavlahy
 int gCasZavlahy[6]; // casy pri ktorych sa prepne dalsia sekcia pri mode 1
 int gPerZavlahy[6]; // percenta pri ktorych sa prepne dalsia sekcia pri modoch 2,3
 int gValueMin, gValueMax;
-int gMode = 0;  // mod zavlahy 0 - nepolievaj, 1 - iba vecer, 2 - vecer aj rano
+int gMode = 1;  // mod zavlahy 0 - nepolievaj, 1 - iba vecer, 2 - vecer aj rano
+int gZostavajuciCasNaplnania = 30;   // cas v minutach kolko sa bude naplnat nadoba, zvoli sa pri spustani
+int lZostavajuciCasNaplnania;   // aktualny cas v sekundach kolko sa bude naplnat nadoba
 
 // ***** funkcie ******
 void zapniSekciu(int i);
@@ -109,6 +112,8 @@ void setup() {
 
   nastavCasPriSpustani();  // nastavenie casu pri spustani
 
+  lZostavajuciCasNaplnania = gZostavajuciCasNaplnania * 60; // cas v sekundach kolko sa bude naplnat nadoba
+
   Status = 0;
 }
 
@@ -134,7 +139,7 @@ void loop() {
         lcd.print("Stop");
         lcd.setCursor(0, 1);
         lcd.print("watering...");
-        delay(10000);
+        delay(1000);
         gZavlaha = 0;
         zapniSekciu(0);
       }
@@ -144,17 +149,17 @@ void loop() {
         lcd.print("Starting");
         lcd.setCursor(0, 1);
         lcd.print("watering...");
-        delay(1000);
+        delay(10000);
         gZavlaha = 1;
         percSekcia = (gPercentoNaplnenia - 3) / 6;
 
         // sekcia 1 vzadu pri domceku
         gPerZavlahy[0] = gPercentoNaplnenia - (percSekcia * 1.6);
-        gCasZavlahy[0] = 15 * 60;
+        gCasZavlahy[0] = 12* 60;
 
         //sekcia 2 pri terase
         gPerZavlahy[1] = gPerZavlahy[0] - (percSekcia * 1.3);
-        gCasZavlahy[1] = 12 * 60;
+        gCasZavlahy[1] = 10 * 60;
 
         //sekcia 3 bok a skalka
         gPerZavlahy[2] = gPerZavlahy[1] - (percSekcia * 0.5);
@@ -162,11 +167,11 @@ void loop() {
 
         //sekcia 4 vpredu rotatory
         gPerZavlahy[3] = gPerZavlahy[2] - (percSekcia * 0.8);
-        gCasZavlahy[3] = 12 * 60;
+        gCasZavlahy[3] = 10 * 60;
 
         // sekcia 5 vzadu pri komposte
         gPerZavlahy[4] = gPerZavlahy[3] - (percSekcia * 1.1);
-        gCasZavlahy[4] = 12 * 60;
+        gCasZavlahy[4] = 10 * 60;
 
         // sekcia 6 kvapkova
         gPerZavlahy[5] = gPerZavlahy[4] - (percSekcia * 0.5);
@@ -208,23 +213,23 @@ void loop() {
 
         // sekcia 1 vzadu pri domceku
         gPerZavlahy[0] = gPercentoNaplnenia - (percSekcia * 1.6);
-        gCasZavlahy[0] = 16 * 60;
+        gCasZavlahy[0] = 12 * 60;
 
         //sekcia 2 pri terase
         gPerZavlahy[1] = gPerZavlahy[0] - (percSekcia * 1.3);
-        gCasZavlahy[1] = 12 * 60;
+        gCasZavlahy[1] = 10 * 60;
 
         //sekcia 3 bok a skalka
         gPerZavlahy[2] = gPerZavlahy[1] - (percSekcia * 0.5);
-        gCasZavlahy[2] = 4 * 60;
+        gCasZavlahy[2] = 3 * 60;
 
         //sekcia 4 vpredu rotatory
         gPerZavlahy[3] = gPerZavlahy[2] - (percSekcia * 0.8);
-        gCasZavlahy[3] = 12 * 60;
+        gCasZavlahy[3] = 10 * 60;
 
         // sekcia 5 vzadu pri komposte
         gPerZavlahy[4] = gPerZavlahy[3] - (percSekcia * 1.1);
-        gCasZavlahy[4] = 11 * 60;
+        gCasZavlahy[4] = 10 * 60;
 
         // sekcia 6 kvapkova
         gPerZavlahy[5] = gPerZavlahy[4] - (percSekcia * 0.5);
@@ -269,6 +274,8 @@ void loop() {
       if (gZavlaha && gCasZavlahy[gZavlaha - 1] > 0) {
         gCasZavlahy[gZavlaha - 1]--;
       }
+      if (fCerpadlo && lZostavajuciCasNaplnania > 0)
+        lZostavajuciCasNaplnania--;   
       prepisDisplay();  // obsluha displeja
       if (sekundOdVypnutiaStudne > 0)
         sekundOdVypnutiaStudne--;
@@ -287,6 +294,7 @@ void loop() {
 void nastavCasPriSpustani() {
   int h = 0, m = 0, s = 0;
   int timer = 0;
+  lcd.clear();
   lcd.begin(16, 2);
   lcd.print("Set hour");
 
@@ -316,6 +324,7 @@ void nastavCasPriSpustani() {
     delay(200);
   }
 
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Set minute");
   timer = 0;
@@ -346,7 +355,7 @@ void nastavCasPriSpustani() {
   }
 
   setTime(h, m, 00, 1, 1, 2016);
-
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Set mode");
   timer = 0;
@@ -364,6 +373,27 @@ void nastavCasPriSpustani() {
       gMode++;
       if (gMode > 2)
         gMode = 0;
+      timer = 0;
+    }
+    else
+      timer++;
+    delay(200);
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Set time to fill");
+  timer = 0;
+  while (timer < 15){
+    // cas
+    lcd.setCursor(0, 1);
+    lcd.print(gZostavajuciCasNaplnania);
+    lcd.print(" ");
+
+    if (digitalRead(13) == 0) {
+      gZostavajuciCasNaplnania++;
+      if (gZostavajuciCasNaplnania > 50)
+        gZostavajuciCasNaplnania = 0;
       timer = 0;
     }
     else
@@ -400,7 +430,7 @@ void zapniSekciu(int k) {
 bool jeVodaNaMax() {
   int sensorValueMax = gValueMax = analogRead(A1);
 
-  if (sensorValueMax < VODA_PRAH)
+  if (sensorValueMax < VODA_PRAH + 100)
     return 1;
   else
     return 0;
@@ -466,6 +496,7 @@ void naplnajNadobu() {
     digitalWrite(12, HIGH); // odpoj kontakty senzorov v studni ak nie su
     senzorStudna = false;
     sekundOdVypnutiaStudne = CAS_STUDNA;
+    lZostavajuciCasNaplnania = gZostavajuciCasNaplnania * 60; // cas v sekundach kolko sa bude naplnat nadoba
     return;   // ak je zavlaha pustena necerpaj vodu do nadoby - vznikali vlny ktore rusili merac hladiny
   }
 
@@ -482,7 +513,8 @@ void naplnajNadobu() {
     }
   
   
-    if (gPercentoNaplnenia >= 100 || !jeVodaNaMin()) { // nadoba plna alebo v studni min elektroda neni zaliata vodou
+    //if (gPercentoNaplnenia >= 100 || !jeVodaNaMin()) { // nadoba plna alebo v studni min elektroda neni zaliata vodou
+    if (lZostavajuciCasNaplnania <= 0 || !jeVodaNaMin()) { // nadoba plna alebo v studni min elektroda neni zaliata vodou
       digitalWrite(11, HIGH); // vypni cerpadlo studne
       fCerpadlo = false;
       digitalWrite(12, HIGH); // vypni kontakt senzorov vlhkosti v studni
@@ -496,7 +528,8 @@ void naplnajNadobu() {
     digitalWrite(12, LOW); // zapni kontakt senzorov vlhkosti v studni
     senzorStudna = true;
     delay(500);
-    if (jeVodaNaMax() && jeVodaNaMin() && (gPercentoNaplnenia <= HLADINA_AKTIVACIE || (gPercentoNaplnenia > HLADINA_AKTIVACIE && gPercentoNaplnenia < 100 && fHladinaBolaMax == false))) {
+    //if (jeVodaNaMax() && jeVodaNaMin() && (gPercentoNaplnenia <= HLADINA_AKTIVACIE || (gPercentoNaplnenia > HLADINA_AKTIVACIE && gPercentoNaplnenia < 100 && fHladinaBolaMax == false))) {
+    if (jeVodaNaMax() && jeVodaNaMin() && lZostavajuciCasNaplnania > 0) {
       digitalWrite(11, LOW); // zapni cerpadlo studne
       fCerpadlo = true;
       return;
@@ -582,6 +615,14 @@ void prepisDisplay(void) {
     lcd.setCursor(9, 1);
     lcd.print(gPerZavlahy[gZavlaha - 1]);
     lcd.print("%<");
+
+     // nadoba
+    if (gPercentoNaplnenia < 100)
+      lcd.setCursor(13, 1);
+    else
+      lcd.setCursor(12, 1);
+    lcd.print(gPercentoNaplnenia);
+    lcd.print("%");
   }
   else {
     if (senzorStudna || (sekundOdVypnutiaStudne >= CAS_STUDNA - 3)){
@@ -590,20 +631,29 @@ void prepisDisplay(void) {
       lcd.print(gValueMax);
       }
     else{
-        lcd.print("- / -");
+        lcd.print(sekundOdVypnutiaStudne);
+        
+        // nadoba
+        //if (gPercentoNaplnenia < 100)
+        //  lcd.setCursor(13, 1);
+        //else
+        //  lcd.setCursor(12, 1);
+        //lcd.print(gPercentoNaplnenia);
+        //lcd.print("%");
       }
-    lcd.print(" ");
-    lcd.print(sekundOdVypnutiaStudne);
+      // ZostavajuciCasNaplnania
+      lcd.setCursor(11, 1);
+      m = lZostavajuciCasNaplnania / 60;
+      s = lZostavajuciCasNaplnania - m * 60;
+      if (m < 10)
+        lcd.print("0");
+      lcd.print(m);
+      lcd.print(":");
+      if (s < 10)
+        lcd.print("0");
+      lcd.print(s);
     }
-  
 
 
-  // nadoba
-  if (gPercentoNaplnenia < 100)
-    lcd.setCursor(13, 1);
-  else
-    lcd.setCursor(12, 1);
-  lcd.print(gPercentoNaplnenia);
-  lcd.print("%");
 
 }
